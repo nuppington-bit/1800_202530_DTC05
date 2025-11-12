7b966c6928493862ca84558359f163810c00cebe
Gurkaren Boyal <gboyal3@my.bcit.ca>
2025-11-11 18:22:45 -0800

    Updates main html and js to not create duplicate ids but use classes instead.

--- src/main.js
+++ src/main.js
@@ -1,50 +1,70 @@
-import { db } from "./firebaseConfig.js"
+import { db } from "./firebaseConfig.js";
 import { onAuthReady } from "./authentication.js";
 import { doc, onSnapshot, getDoc } from "firebase/firestore";
-import { collection, getDocs, addDoc, serverTimestamp, query, orderBy, where } from "firebase/firestore";
+import {
+  collection,
+  getDocs,
+  addDoc,
+  serverTimestamp,
+  query,
+  orderBy,
+  where,
+} from "firebase/firestore";
 
+function showUsersBookmarks() {
+  onAuthReady(async (user) => {
+    if (!user) {
+      location.href = "index.html";
+      return;
+    }
+  });
+}
+function addBookmark() {}
 async function displayArticleCardsDynamically(params) {
-    onAuthReady(async (user) => {
-        if (!user) {
-            location.href = "index.html";
-            return;
-        }
-        let userKnowledgeLevel = 0
-        let cardTemplate = document.getElementById("article_card_template");
-        const usersCollectionRef = doc(db, "users", user.uid);
-        try {
-            const querySnapshot = await getDoc(usersCollectionRef);
-            userKnowledgeLevel = querySnapshot.data().knowledgeLevel
-        }
-        catch (error) {
-            console.log(error)
-        }
-        const articlesCollectionRef = query(collection(db, "articles"), orderBy("article_name", "asc"), where("level", "==", userKnowledgeLevel));
+  onAuthReady(async (user) => {
+    if (!user) {
+      location.href = "index.html";
+      return;
+    }
+    let userKnowledgeLevel = 0;
+    let cardTemplate = document.getElementById("article_card_template");
+    const usersCollectionRef = doc(db, "users", user.uid);
+    try {
+      const querySnapshot = await getDoc(usersCollectionRef);
+      userKnowledgeLevel = querySnapshot.data().knowledgeLevel;
+    } catch (error) {
+      console.log(error);
+    }
+    const articlesCollectionRef = query(
+      collection(db, "articles"),
+      orderBy("article_name", "asc"),
+      where("level", "==", userKnowledgeLevel)
+    );
 
-
-        try {
-            const querySnapshot = await getDocs(articlesCollectionRef);
-            querySnapshot.forEach(doc => {
-                let newcard = cardTemplate.content.cloneNode(true);
-                const article = doc.data();
-                console.log(article);
-                if (article.level + 1 > 1) {
-                    newcard.querySelector("#brain-2").setAttribute("fill", "#000")
-                }
-                if (article.level + 1 > 2) {
-                    newcard.querySelector("#brain-3").setAttribute("fill", "#000")
-                }
-                newcard.querySelector("#article_name").textContent = article.article_name;
-                newcard.querySelector("#summary_text").textContent = article.summary;
-                newcard.querySelector("#article_link").href = article.link;
-                newcard.querySelector("#article_link_title").href = article.link;
-
-                document.getElementById("articles_go_here").appendChild(newcard);
-            })
-        } catch (error) {
-            console.error("error getting documents", error);
+    try {
+      const querySnapshot = await getDocs(articlesCollectionRef);
+      querySnapshot.forEach((doc) => {
+        let newcard = cardTemplate.content.cloneNode(true);
+        const article = doc.data();
+        console.log(article);
+        if (article.level + 1 > 1) {
+          newcard.querySelector("#brain-2").setAttribute("fill", "#000");
         }
-    })
+        if (article.level + 1 > 2) {
+          newcard.querySelector("#brain-3").setAttribute("fill", "#000");
+        }
+        newcard.querySelector(".article_name").textContent =
+          article.article_name;
+        newcard.querySelector(".summary_text").textContent = article.summary;
+        newcard.querySelector(".article_link").href = article.link;
+        newcard.querySelector(".article_link_title").href = article.link;
+
+        document.getElementById("articles_go_here").appendChild(newcard);
+      });
+    } catch (error) {
+      console.error("error getting documents", error);
+    }
+  });
 }
 
 displayArticleCardsDynamically();
